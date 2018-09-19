00be7f705e31188721fcbe571a9819aed37c0093
unknown <thomasprost@warpjapan.com>
2018-09-19 14:41:43 +0900

    inital commit with an example structure in src and configuration files.

---
+++ src/scripts/app.js
@@ -0,0 +1,9 @@
+import {getWelcomeText} from './app/console'
+
+console.log(getWelcomeText("WarpJapan"))
+
+$(function () {
+  console.log('Example of using jQuery thanks to modules')
+})
+
+
---
+++ src/scripts/app/console.js
@@ -0,0 +1,8 @@
+const textSample = "Zorg"
+
+export function getWelcomeText(text = textSample){
+  // Just a dummy function
+  // Remove it and start creating your own module
+  return `Welcome to ${text} 's boilerplate`
+}
+
